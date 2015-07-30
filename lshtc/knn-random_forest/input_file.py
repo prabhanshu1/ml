5ad95239c51e46614dd7aa2088b264ebf3ae4e40
Prabhanshu Abhishek <pabhi@iitk.ac.in>
2015-07-31 00:01:26 +0530

    made two version or read_file function for two different parsing method used

--- lshtc/knn-random_forest/input_file.py
+++ lshtc/knn-random_forest/input_file.py
@@ -32,10 +32,9 @@ def read_file(filename,ids):
             for ro in reader:
                 label.append(ro)
                 x=label[i][len(label[i])-1].split()
-                label[i][len(label[i])-1]=int(x[0])
+                del label[i][len(label[i])-1]
+                
                 label[i]=[int(i) for i in label[i]]
-                del x[0]
-
                 x=[line.split(':',1) for line in x]
                 for l in range(len(x)):
                     row.append(int(i))
@@ -46,7 +45,7 @@ def read_file(filename,ids):
                     data.append(asd)
                 i=i+1    
             globals.length=i;
-            globals.max_col=max_col;
+           
             if ids is 0:
                 globals.train_label=label; label=[];
                 return csr_matrix( (data,(row,col)), shape=(i,globals.max_col+1),dtype=int16)
@@ -64,7 +63,45 @@ def read_file(filename,ids):
 
 
 
+def read_file1(filename,ids):
+    row=[];
+    col=[];
+    data=[]
+    max_col=0;
+    label=[]
+    i=0;
+    
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        try:
+            for ro in reader:
+                label.append(ro)
+                x=label[i][len(label[i])-1].split()
+                label[i][len(label[i])-1]=int(x[0])
+                label[i]=[int(i) for i in label[i]]
+                del x[0]
 
+                x=[line.split(':',1) for line in x]
+                for l in range(len(x)):
+                    row.append(int(i))
+                    col.append(int(x[l][0]))
+                    if max_col< int(x[l][0]):
+                        max_col=int(x[l][0])
+                    asd=int(int(x[l][1])*globals.idf[int(x[l][0])])
+                    data.append(asd)
+                i=i+1    
+            globals.length=i;
 
+            if ids is 0:
+                globals.train_label=label; label=[];
+                return csr_matrix( (data,(row,col)), shape=(i,globals.max_col+1),dtype=int16)
+                row=[];col=[];data=[];
+
+            else:
+                globals.cros_label=label;label=[]
+                return csr_matrix( (data,(row,col)), shape=(i,globals.max_col+1),dtype=int16)
+                row=[];col=[];data=[];
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 
 
