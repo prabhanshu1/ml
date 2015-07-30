3b98a8b7086069574d4aeb912eb8d9c34d60fd2a
Prabhanshu Abhishek <pabhi@iitk.ac.in>
2015-07-30 23:14:17 +0530

    global variables used in the knn and random forest implementation

---
+++ lshtc/knn-random_forest/globals.py
@@ -0,0 +1,19 @@
+
+
+def init():
+    global train_label
+    global cros_label
+    global row
+    global col
+    global data
+    global max_col
+    global idf
+    global length
+    train_label=[]
+    cros_label=[]
+    row=[]
+    col=[]
+    data=[]
+    max_col=2086000;
+    idf=[0]*2086000
+    length=0
