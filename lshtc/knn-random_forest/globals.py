b15b2c8ce06d6d86b72b2f3cb2acb0dce3a9dc45
Prabhanshu abhishek <pabhi@cseproj116.cse.iitk.ac.in>
2015-08-13 20:05:00 +0530

    addedd offset in globals, which stores the offset for seeking file to the write position, changed the read function which accepts offset , and process only 1000 lines

--- lshtc/knn-random_forest/globals.py
+++ lshtc/knn-random_forest/globals.py
@@ -9,6 +9,16 @@ def init():
     global max_col
     global idf
     global length
+    global offset
+    global true_positive
+    global true_negative
+    global false_positive
+    global false_negative
+    true_positive=0
+    true_negative=0
+    false_positive=0
+    false_negative=0
+    offset=0
     train_label=[]
     cros_label=[]
     row=[]
--- lshtc/knn-random_forest/input_file.py
+++ lshtc/knn-random_forest/input_file.py
@@ -7,7 +7,7 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn import *
 #from scipy.spatial.distance import *
 import globals
-
+import math
 
 def read_idf(idf_file):
     with open(idf_file,newline='') as f:
@@ -15,10 +15,10 @@ def read_idf(idf_file):
             a,b= line.split()
             a=int(a);
             b=float(b);
-            globals.idf[a]=int(b);
+            globals.idf[a]=int(math.log2(b));
 
 
-def read_file(filename,ids):
+def read_file(filename,ids,offset):
     row=[];
     col=[];
     data=[]
@@ -27,12 +27,20 @@ def read_file(filename,ids):
     i=0;
     
     with open(filename, newline='') as f:
+        f.seek(offset);
         reader = csv.reader(f)
+        #reader =f.readlines()
         try:
             for ro in reader:
                 label.append(ro)
+                globals.offset+=(sum(len(s)+1 for s in ro))
                 x=label[i][len(label[i])-1].split()
+                label[i][len(label[i])-1]=0
                 del label[i][len(label[i])-1]
+
+                if len(x) is 0 :
+                    del label[i]
+                    continue
                 
                 label[i]=[int(i) for i in label[i]]
                 x=[line.split(':',1) for line in x]
@@ -43,9 +51,12 @@ def read_file(filename,ids):
                         max_col=int(x[l][0])
                     asd=int(int(x[l][1])*globals.idf[int(x[l][0])])
                     data.append(asd)
-                i=i+1    
+                i=i+1
+                if ids ==1 and i>= 1000:
+                    break;
             globals.length=i;
-           
+            if i == 0:
+                return 0;
             if ids is 0:
                 globals.train_label=label; label=[];
                 return csr_matrix( (data,(row,col)), shape=(i,globals.max_col+1),dtype=int16)
@@ -80,7 +91,10 @@ def read_file1(filename,ids):
                 label[i][len(label[i])-1]=int(x[0])
                 label[i]=[int(i) for i in label[i]]
                 del x[0]
-
+                if len(x)is 0:
+                    del label[i]
+                    continue
+                
                 x=[line.split(':',1) for line in x]
                 for l in range(len(x)):
                     row.append(int(i))
